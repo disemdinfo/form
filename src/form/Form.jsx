652e10e0b6cb18e0a8180e668f0b8a5fec3f4135
F4103757 Henrique Fidellis Ferreira <fidellis@bb.com.br>
2019-06-11 14:16:49 -0300

    alt

--- src/form/Form.jsx
+++ src/form/Form.jsx
@@ -1,86 +1,79 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Form.css';
 import Button from './Button.jsx';
 
-function getError({ id, value, error, required, min, max }){
-  
-  if(required && value !== 0){
-    const text = 'Campo obrigatório.';
-    if(Array.isArray(value)){
-      if(!value.length) return text;     
-    } else {
-      if(!value) return text;
-    }   
-  }
+function getError({ id, value, error, required, min, max }) {
+  if (id) {
+    if (required && value !== 0) {
+      const text = 'Campo obrigatório.';
+      if (Array.isArray(value)) {
+        if (!value.length) return text;
+      } else if (!value) return text;
+    }
 
-  if(min){
-    if(value < min) return `Mínimo ${min}.`;  
-  }
+    if (min) {
+      if (value < min) return `Mínimo ${min}.`;
+    }
 
-  if(max){
-    if(value > max) return `Máximo ${max}.`;  
-  }
+    if (max) {
+      if (value > max) return `Máximo ${max}.`;
+    }
 
-  if(error){
-    return error()
+    if (error) {
+      return error();
+    }
   }
 
   return null;
 }
 
-function getErrors(children){
+function getErrors(children) {
   return children.map(c => ({
     ...c,
-    props: { 
-      ...c.props, 
-      error: getError(c.props) 
-    }      
+    props: {
+      ...c.props,
+      error: getError(c.props),
+    },
   }));
 }
 
 class Form extends Component {
+  constructor(props) {
+    super(props);
 
-  constructor(props){
-    super(props)
-    
     const { children } = props;
 
     this.state = {
       children,
     };
 
-    this.onSubimit = this.onSubimit.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
-  componentWillReceiveProps({ children }){
-    if(children !== this.props.children){      
-     this.setState({ children: this.state.submited ? getErrors(children) : children });
+  componentWillReceiveProps({ children }) {
+    if (children !== this.props.children) {
+      this.setState({ children: this.state.submited ? getErrors(children) : children });
     }
   }
 
-  onSubimit(){
-
+  onSubmit() {
     const children = getErrors(this.props.children);
     const isValid = children.every(c => !c.props.error);
-
-    if(isValid){
-      this.props.onSubimit();
-    } else {
-      this.setState({ children, submited: true });
-    }
+    this.setState({ children, submited: true }, () => this.props.onSubmit({ isValid }));
   }
 
   render() {
-    const { actions, onSubimit, ...props } = this.props;
-    const { children } = this.state;    
-    
+    const { actions, onSubmit, width, style, ...props } = this.props;
+    const { children } = this.state;
+
     return (
-      <div className="container" {...props}>
+      <div className="container" style={{ width, ...style }} {...props}>
         <div className="form">
-          {children}          
+          {children}
         </div>
         <div className="actions">
-          <Button label="Salvar" onClick={this.onSubimit} />
+          <Button label="Salvar" onClick={onSubmit ? this.onSubmit : null} />
           {actions.map((action, key) => <Button key={key} {...action} />)}
         </div>
       </div>
@@ -88,8 +81,19 @@ class Form extends Component {
   }
 }
 
+Form.propTypes = {
+  children: PropTypes.node.isRequired,
+  onSubmit: PropTypes.func,
+  actions: PropTypes.array,
+  width: PropTypes.string,
+  style: PropTypes.object,
+};
+
 Form.defaultProps = {
-  actions: []
-}
+  onSubmit: null,
+  actions: [],
+  width: '100%',
+  style: {},
+};
 
 export default Form;
--- src/form/Select.jsx
+++ src/form/Select.jsx
@@ -1,46 +1,49 @@
 import React, { PureComponent } from 'react';
-import Container from './Container'
-import Select from "react-virtualized-select";
-import "react-select/dist/react-select.css";
-import "react-virtualized-select/styles.css";
+import Container from './Container';
+import Select from 'react-virtualized-select';
+import 'react-select/dist/react-select.css';
+import 'react-virtualized-select/styles.css';
+
+
+function convertOptions({ options, optionValue, optionLabel }) {
+  return options.map(option => ({ value: option[optionValue], label: option[optionLabel] }));
+}
 
 class SelectCheckbox extends PureComponent {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            values: props.value,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    render(){
-        const { multi } = this.props;
-        const { values } = this.state;
 
-        return (
-            <Container {...this.props} > 
-                {({ options, onChange, id, ...inputProps }) => 
-                <Select 
-                    {...inputProps}                    
-                    searchable
-                    value={values}                    
-                    options={options}
-                    ignoreAccents                    
-                    onChange={i => {
-                        const item = multi ?  i || [] : i || {};
-                        this.setState({ values: multi ? item.map(i => i.value) : item.value }, () => onChange({ id, value: this.state.values }))}
-                    }                                        
-                    clearAllText="Remover todos"
-                    searchPromptText="Digite o que procura"
-                    noResultsText="Nenhum resultado encontrado."                    
-                />}
-            </Container>)
-    }
-};
+  render() {
+    const { multi, value } = this.props;
+
+    return (
+      <Container {...this.props} >
+        {({ options, onChange, id, optionValue, optionLabel, ...inputProps }) => (<Select
+          {...inputProps}
+          value={value}
+          options={convertOptions({ options, optionValue, optionLabel })}
+          ignoreAccents
+          onChange={(i) => {
+            const item = multi ? i || [] : i || {};
+            onChange({ id, value: multi ? item.map(i => i.value) : item.value });
+          }}
+          clearAllText="Remover todos"
+          searchPromptText="Digite o que procura"
+          noResultsText="Nenhum resultado encontrado."
+        />)}
+      </Container>);
+  }
+}
 
 SelectCheckbox.defaultProps = {
   options: [],
-  multi: false
+  searchable: true,
+  multi: false,
+  optionValue: 'value',
+  optionLabel: 'label',
 };
 
 export default SelectCheckbox;
