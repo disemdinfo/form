e13aebb827edc3683c06427d85f086eecf67ab4f
F4103757 Henrique Fidellis Ferreira <fidellis@bb.com.br>
2019-06-14 11:30:34 -0300

    alt

--- src/form/SnackBar.jsx
+++ src/form/SnackBar.jsx
@@ -18,17 +18,17 @@ export default class SnackBar extends Component<Props, State> {
     super(props);
     this.state = {
       showSnackBar: this.props.show,
-      timer: this.props.timer
+      timer: this.props.timer,
     };
   }
 
   componentWillReceiveProps(nextProps: Object) {
-    var {showSnackBar, timer} = this.state;
+    const { showSnackBar, timer } = this.state;
     if (showSnackBar !== nextProps.show) {
       this.setState({
         showSnackBar: nextProps.show,
-        timer: nextProps.timer
-      }, () => {          
+        timer: nextProps.timer,
+      }, () => {
         setTimeout(() => {
           this.setState({ showSnackBar: false });
           this.props.onHide();
@@ -37,14 +37,14 @@ export default class SnackBar extends Component<Props, State> {
     }
   }
 
-  render() {    
+  render() {
     const { showSnackBar } = this.state;
-    
+
     const container = {
       position: 'fixed',
       left: '0px',
       bottom: '20px',
-      width: '100%',      
+      width: '100%',
       WebkitTransition: 'translate 0.3s cubic-bezier(0, 0, 0.30, 1)',
       transition: 'translate 0.3s cubic-bezier(0, 0, 0.30, 1)',
       fontWeight: '500',
@@ -52,52 +52,56 @@ export default class SnackBar extends Component<Props, State> {
       willChange: 'transform',
       whiteSpace: 'nowrap',
       transform: 'translateY(20px)',
-      WebkitTransform: 'translateY(20px)',      
+      WebkitTransform: 'translateY(20px)',
       fontSize: '14px',
-      opacity: 0,      
+      display: 'none',
+      // opacity: 0,
       // display: '-webkit-box',
       // display: '-ms-flexbox',
-      display: 'flex',
+      // display: 'flex',
       WebkitBoxAlign: 'center',
       msFlexAlign: 'center',
       alignItems: 'center',
       WebkitBoxPack: 'justify',
       msFlexPack: 'justify',
       justifyContent: 'space-between',
-      lineHeight: '20px',        
+      lineHeight: '20px',
     };
 
-    const snackbarStyle = { 
-        background: '#404040',
-        marginLeft: 'auto', 
-        marginRight: 'auto',
-        borderRadius: '3px',
-        color: '#fff',
-        paddingRight: '24px',
-        paddingLeft: '24px',
-        textAlign: 'center',
-        boxShadow: '0 0 2px rgba(0,0,0,.12), 0 2px 4px rgba(0,0,0,.24)',
-        minWidth: 200    
+    const snackbarStyle = {
+      background: '#404040',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      borderRadius: '3px',
+      color: '#fff',
+      paddingRight: '24px',
+      paddingLeft: '24px',
+      paddingTop: '24px',
+      paddingBottom: '24px',
+      textAlign: 'center',
+      boxShadow: '0 0 2px rgba(0,0,0,.12), 0 2px 4px rgba(0,0,0,.24)',
+      minWidth: 200,
     };
 
     if (showSnackBar) {
-      container.opacity = 1;
+      container.display = 'flex';
+      // container.opacity = 1;
       container.transform = 'translateY(0)';
     }
 
     return (
       <div style={container}>
-            <div style={snackbarStyle}>
-                {this.props.children}
-            </div>
+        <div style={snackbarStyle}>
+          {this.props.children}
+        </div>
       </div>
     );
   }
-};
+}
 
 SnackBar.propTypes = {
   show: PropTypes.bool.isRequired,
-  timer: PropTypes.number
+  timer: PropTypes.number,
 };
 
 SnackBar.defaultProps = {
