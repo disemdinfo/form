81da058c6c6c0beebe22e5ad79b94723c61a2c7d
Henrique Fidellis <fidellis@bb.com.br>
2020-07-01 07:11:15 -0300

    test

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import SelectAsync from './inputs/SelectAsync';
 import Switch from './inputs/Switch';
 import TextArea from './inputs/TextArea';
 import TextInput from './inputs/TextInput';
+import Label from './inputs/Label';
 import Button from './Button';
 
-export { Form, DateInput, NumberInput, Radio, Select, SelectCheckbox, SelectAsync, Switch, TextArea, TextInput, Button };
+export { Form, DateInput, NumberInput, Radio, Select, SelectCheckbox, SelectAsync, Switch, TextArea, TextInput, Label, Button };
 export default Form;
