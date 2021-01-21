30ef24b56fcd5c9dfa64d1dd07a816c48b0a9ab1
Henrique Fidellis <fidellis@bb.com.br>
2021-01-20 22:42:17 -0300

    alt

--- src/inputs/DateInput.js
+++ src/inputs/DateInput.js
@@ -4,21 +4,22 @@ import moment from 'moment';
 import 'react-datetime/css/react-datetime.css';
 import ComponentContainer from './ComponentContainer';
 
-const DateInput = ({ label, onChange, id, value, closeOnSelect, timeFormat, isValidDate, minDate, maxDate, ...inputProps }) => (
+const DateInput = ({ label, onChange, id, value, closeOnSelect, timeFormat, isValidDate, minDate, maxDate, ...props }) => (
   <ComponentContainer label={label}>
     <Datetime
-      id={id}
-      value={value ? moment(value).utc() : null}
-      closeOnSelect={closeOnSelect}
-      timeFormat={timeFormat}
-      isValidDate={(current) => {
-        const isValidMin = minDate ? current > minDate : true;
-        const isValidMax = maxDate ? current <= maxDate : true;
+      {...props}
+      // id={id}
+      // value={value ? moment(value) : null}
+      // closeOnSelect={closeOnSelect}
+      // timeFormat={timeFormat}
+      // isValidDate={(current) => {
+      //   const isValidMin = minDate ? current > minDate : true;
+      //   const isValidMax = maxDate ? current <= maxDate : true;
 
-        return isValidDate(current) && isValidMin && isValidMax;
-      }}
-      onChange={date => onChange({ id, value: date })}
-      inputProps={{ className: 'input input-date', ...inputProps }}
+      //   return isValidDate(current) && isValidMin && isValidMax;
+      // }}
+      // onChange={date => onChange({ id, value: date })}
+      inputProps={{ className: 'input input-date' }}
     />
   </ComponentContainer>
 );
