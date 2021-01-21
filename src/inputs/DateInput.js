aeb4a0f1e221f30c6c854d1f55a47ef228578bee
Henrique Fidellis <fidellis@bb.com.br>
2021-01-20 23:40:54 -0300

    alt

--- src/inputs/DateInput.js
+++ src/inputs/DateInput.js
@@ -4,31 +4,37 @@ import moment from 'moment';
 import 'react-datetime/css/react-datetime.css';
 import ComponentContainer from './ComponentContainer';
 
-const DateInput = ({ label, onChange, id, value, closeOnSelect, timeFormat, isValidDate, minDate, maxDate, ...inputProps }) => (
-  <ComponentContainer label={label}>
-    {/* <Datetime
-      id={id}
-      value={value ? moment(value).utc() : null}
-      closeOnSelect={closeOnSelect}
-      timeFormat={timeFormat}
-      isValidDate={(current) => {
-        const isValidMin = minDate ? current > minDate : true;
-        const isValidMax = maxDate ? current <= maxDate : true;
+// const DateInput = ({ label, onChange, id, value, closeOnSelect, timeFormat, isValidDate, minDate, maxDate, ...inputProps }) => (
+//   <ComponentContainer label={label}>
+//     <Datetime
+//       id={id}
+//       value={value ? moment(value).utc() : null}
+//       closeOnSelect={closeOnSelect}
+//       timeFormat={timeFormat}
+//       isValidDate={(current) => {
+//         const isValidMin = minDate ? current > minDate : true;
+//         const isValidMax = maxDate ? current <= maxDate : true;
 
-        return isValidDate(current) && isValidMin && isValidMax;
-      }}
-      onChange={date => onChange({ id, value: date })}
-      inputProps={{ className: 'input input-date', ...inputProps }}
-    /> */}
+//         return isValidDate(current) && isValidMin && isValidMax;
+//       }}
+//       onChange={date => onChange({ id, value: date })}
+//       inputProps={{ className: 'input input-date', ...inputProps }}
+//     />
+//   </ComponentContainer>
+// );
+
+const DateInput = ({ label, onChange, id, ...props }) => (
+  <ComponentContainer label={label}>
     <input
       {...props}
       type="date"
       id={id}
       onChange={e => onChange({ id, value: e.target.value })}
       className='input input-date'
-    /> 
+      /> 
   </ComponentContainer>
 );
+
 
 DateInput.defaultProps = {
   closeOnSelect: true,
